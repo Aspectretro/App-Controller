3976100a8df320cb8ec8658ff4d12d4fbc10a261
Aspectretro <max.zhengsc10@gmail.com>
2025-07-14 12:28:26 +1000

    feat: main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.title('Tkinter Window Demo')
+window.geometry('600x400+50+50')
+window.attributes('-topmost', 1)
+# TODO: add icon
+
+# Labels
+
+
+window.mainloop()
