4e5ceb7ba34ec48480dc477149f3063416890254
Aspectretro <max.zhengsc10@gmail.com>
2025-07-17 13:10:12 +1000

    feat: buttons for chrome, music, and word

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import os
+from PIL import Image, ImageTk
 
 """
 Planning:
@@ -16,17 +17,59 @@ Extension:
 """
 
 class App(tk.Tk): # Window configuration
+    def click(self):
+        print("Clicked")
+
     def __init__(self):
         super().__init__()
 
         self.title("App Controller")
         self.geometry("500x400")
 
+        # Heading
         self.label = ttk.Label(self, text="Main Control", font=("Times New Roman", 13))       
         self.label.pack()
 
+        # Image
+        originalWord = Image.open("./assets/Word.png")
+        originalChrome = Image.open("./assets/Chrome.png")
+        originalMusic = Image.open("./assets/Music.png")
 
-        # Font configure
+        resizeWord = originalWord.resize((50,50), Image.LANCZOS)
+        resizeChrome = originalChrome.resize((50,50), Image.LANCZOS)
+        resizeMusic = originalMusic.resize((50,50), Image.LANCZOS)
+
+        # Icon
+        self.word_icon = ImageTk.PhotoImage(resizeWord)
+        self.chrome_icon = ImageTk.PhotoImage(resizeChrome)
+        self.music_icon = ImageTk.PhotoImage(resizeMusic)
+
+        # Word
+        self.word = ttk.Button(self, 
+                               image=self.word_icon, 
+                               command=self.click)
+        self.word.pack(
+            ipadx=5,
+            ipady=5,
+        )
+
+        # Chrome
+        self.chrome = ttk.Button(self,
+                                 image = self.chrome_icon,
+                                 command = self.click)
+        self.chrome.pack(
+            ipadx=5,
+            ipady=5
+        )
+        
+        # Music
+        self.music = ttk.Button(self,
+                                image = self.music_icon,
+                                command = self.click)
+        self.music.pack(
+            ipadx=5,
+            ipady=5
+        )
 
 if __name__ == "__main__":
     app = App()
