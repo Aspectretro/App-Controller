29f0fe1842bf355af43feacd9b90c3121a4995c6
Aspectretro <max.zhengsc10@gmail.com>
2025-07-21 10:15:29 +1000

    fix: revert back to having google chrome as default application.
    After this change, need to make notes on info file to inform that the browser this application opens is chrome.

--- src/control.py
+++ src/control.py
@@ -1,27 +1,9 @@
 from AppOpener import open
 import webbrowser
-import winreg
 
 class ctrl():
     def __init__(self):
         self.name = None
-        self.browser_id = {
-            "ChromeHTML": "google chrome",
-            "MSEdgeHTM": "microsoft edge",
-            "FirefoxURL": "firefox",
-            "OperaStable": "opera gx browser",
-        }
-    
-    def get_default_browser_windows(self) -> str:
-        try:
-            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, 
-                            r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice") as key:
-                browser = winreg.QueryValueEx(key, 'ProgId')[0]
-                return self.browser_map.get(browser) 
-        except Exception:
-            return None
-
-
     def setName(self, name):
         self.name = name
     
@@ -31,13 +13,5 @@ class ctrl():
     def press(self, name): # TODO: obtain information from os to find default browser
         if not name:
             return False
-        try:
-            if name.lower() in ["google chrome", "microsoft edge", "firefox", "opera gx browser"]:
-                default_brower = self.get_default_browser_windows()
-                open(default_brower)
-            else:
-                open(name.lower())
-        except Exception as e:
-            print(f"Error opening application '{name}': {e}")
-            if "browser" in name.lower():
-                webbrowser.open("about: blank")
+        else:
+            open(name.lower())
--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ class App(tk.Tk): # Window configuration
         # Buttons
         buttons = [
             ("word", 0, lambda: self.click("word")),
-            ("chrome", 1, self.browser),
+            ("chrome", 1, lambda: self.click("google chrome")),
             ("music", 2, lambda: self.click("qq music")),
             ("file", 3, lambda: self.click("file explorer")),
             ("settings", 4, lambda: self.click("settings"))
@@ -49,11 +49,6 @@ class App(tk.Tk): # Window configuration
 
         # FIXME: Temp exit button for testing
         self.exit = ttk.Button(self, text="Exit", command=lambda: self.quit()).grid(column=0, row= 4)
-
-    def browser(self): # This is for browser ONLY
-        browser_name = self.command.get_default_browser_windows()
-        print(browser_name)
-        self.command.press(browser_name)
     
     def click(self, name):
         try:
@@ -64,86 +59,3 @@ class App(tk.Tk): # Window configuration
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-
-
-
-# originalWord = Image.open("./assets/Word.png")
-#         originalChrome = Image.open("./assets/Chrome.png")
-#         originalMusic = Image.open("./assets/Music.png")
-#         originalFile = Image.open("./assets/File.png") 
-#         originalSetting = Image.open("./assets/Settings.png")
-
-#         resizeWord = originalWord.resize((50,50), Image.LANCZOS)
-#         resizeChrome = originalChrome.resize((50,50), Image.LANCZOS)
-#         resizeMusic = originalMusic.resize((50,50), Image.LANCZOS)
-#         resizeFile = originalFile.resize((50,50), Image.LANCZOS)
-#         resizeSetting = originalSetting.resize((50,50), Image.LANCZOS)
-
-#         # Icon
-#         self.word_icon = ImageTk.PhotoImage(resizeWord)
-#         self.chrome_icon = ImageTk.PhotoImage(resizeChrome)
-#         self.music_icon = ImageTk.PhotoImage(resizeMusic)
-#         self.file_icon = ImageTk.PhotoImage(resizeFile)
-#         self.setting_icon = ImageTk.PhotoImage(resizeSetting)
-
-#         # Word
-#         self.word = ttk.Button(self, 
-#                                image=self.word_icon, 
-#                                command = lambda: self.click("word")
-#                               )
-#         self.word.grid(
-#             column=0,
-#             row=1,
-#             padx=5,
-#             pady=5
-#         )
-
-#         # Chrome
-#         self.chrome = ttk.Button(self,
-#                                  image = self.chrome_icon,
-#                                  command = self.browser
-#                                  )
-#         self.chrome.grid(
-#             column=1,
-#             row=1,
-#             padx=5,
-#             pady=5
-#         )
-        
-#         # Music
-#         self.music = ttk.Button(self,
-#                                 image = self.music_icon,
-#                                 command = lambda: self.click("music")
-#                                 )
-#         self.music.grid(
-#             column=2,
-#             row=1,
-#             padx=5,
-#             pady=5
-#         )
-
-#         # File Explorer
-#         self.file = ttk.Button(self,
-#                                image = self.file_icon,
-#                                command = lambda: self.click("file explorer")
-#                                )
-        
-#         self.file.grid(
-#             column=3,
-#             row=1,
-#             padx=5,
-#             pady=5
-#         )
-
-#         # Setting
-#         self.settings = ttk.Button(self, 
-#                                    image = self.setting_icon, 
-#                                    command=lambda: self.click("settings")
-#                                    )
-#         self.settings.grid(
-#             column=4,
-#             row=1,
-#             padx=5,
-#             pady=5
-#         )
